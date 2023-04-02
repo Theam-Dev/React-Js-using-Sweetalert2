2e9a49667d554665e5127468a2ee26af69450af4
theam-dev <phearakroun@gmail.com>
2023-04-02 20:23:30 +0700

    Sweetalert

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import Swal from "sweetalert2";
+export default class App extends Component{
+  constructor() {
+    super();
+    this.Success = this.Success.bind(this);
+    this.YesNo = this.YesNo.bind(this);
+  }
+  Success() {
+    Swal.fire({
+      title: "Success",
+      type: "success",
+      text: "Your work has been saved.",
+    });
+  }
+  YesNo(){
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire(
+          'Deleted!',
+          'Your file has been deleted.',
+          'success'
+        )
+      }
+    })
+  }
+  render() {
+    return (
+      <div style={{padding:"20px"}}>
+          <h2>SweetAlert2 In React</h2>
+        <br/>
+        <div style={{ paddingTop: "10px" }}>
+          <button className="btn btn-info btn" onClick={this.Success}>
+            Success
+          </button>|
+          <button className="btn btn-info btn" onClick={this.YesNo}>
+            Yes No
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
